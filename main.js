8fe781f088b0915830b952f94366acd358a0d717
briancatraguna <briancatraguna@rocketmail.com>
2021-07-07 01:35:17 +0700

    Submission of first homework

---
+++ main.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+    const message = "The title is " + title + " and the description is " + description + ".";
+    alert(message);
+}
