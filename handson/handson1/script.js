8e54b86956917b78bf461d7756f01a5d05f2af67
briancatraguna <briancatraguna@rocketmail.com>
2021-07-09 12:10:27 +0700

    Fixed script issue in html

---
+++ handson/handson1/script.js
@@ -0,0 +1,10 @@
+const students = [
+    { name: "Alice", age: 20},
+    { name: "Bob", age: 21},
+    { name: "John", age: 20},
+    { name: "Jane", age: 20},
+];
+
+let table = document.getElementById("myTable");
+
+console.log(table);
